c216c22b2e7d80b5215b041cd54eae42df8a02d1
Nhan <natenguyen51294@gmail.com>
2019-12-20 08:11:43 -0800

    render characters on the screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharacterList from './components/CharacterList';
+import WelcomePage from './components/WelcomePage';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <main>
-      <Header />
+          <Header />
+          <CharacterList />
+          <WelcomePage />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from './CharacterCard';
+import styled from 'styled-components';
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
+
+  const ListStyling = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+  `;
 
   useEffect(() => {
+    axios.get(`https://rickandmortyapi.com/api/character/`)
+      .then(res => {
+        setCharacters(res.data.results);
+        console.log(res.data.results);
+      })
+      .catch(err => console.log(err));
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
+  // console.log(characters);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <ListStyling className="character-list">
+      {characters.map((character, index) => {
+        return <CharacterCard key={index} name={character.name} img={character.image} gender={character.gender} species={character.species} />;
+      })}
+    </ListStyling>
   );
 }
